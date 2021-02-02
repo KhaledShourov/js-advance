8726181429be580b08d646a980110682854bb40c
Khaled Shourov <khaledwebbd@gmail.com>
2021-02-02 12:52:43 +0600

    js truthy or falsy

---
+++ trueFalse.js
@@ -0,0 +1,16 @@
+//Give that False Output /Falsy
+// 0
+// ""
+// null
+// undefined
+// NaN
+
+//Give that False Output /Truthy
+// '0 ', ' ' []
+let name;
+console.log(name);
+if(name){
+    console.log("Condition is True");
+}else{
+    console.log("Condition is False");
+}
