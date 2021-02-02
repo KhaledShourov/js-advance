8ac088aaf886cd8811b1ecf66273d20b51db7c6b
Khaled Shourov <khaledwebbd@gmail.com>
2021-02-02 20:48:12 +0600

    break continue

---
+++ scope.js
@@ -0,0 +1,8 @@
+function sum (first , second ){
+    let result = first + second;
+    console.log(result);
+    return result;
+}
+
+const output = sum(10, 10);
+console.log(output);
---
+++ slice-splice.js
@@ -0,0 +1,9 @@
+const nums = [1,2,3.4,5,6];
+
+const remove = nums.splice(2,3,77);
+// console.log(remove);
+// console.log(nums);
+
+const together = nums.join("");
+console.log(together);
+
